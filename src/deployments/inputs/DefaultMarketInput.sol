2f7d255343713adb75cc411252b16d08067b5628
fbslo <admin@fbslo.net>
2025-02-21 02:23:06 +0100

    feat: HyperLend deployment

--- src/deployments/inputs/DefaultMarketInput.sol
+++ src/deployments/inputs/DefaultMarketInput.sol
@@ -20,11 +20,15 @@ contract DefaultMarketInput is MarketInput {
     roles.emergencyAdmin = deployer;
     roles.poolAdmin = deployer;
 
-    config.marketId = 'Aave V3 Testnet Market';
-    config.providerId = 8080;
+    config.marketId = 'HyperLend Market';
+    config.providerId = 1;
     config.oracleDecimals = 8;
     config.flashLoanPremiumTotal = 0.0005e4;
     config.flashLoanPremiumToProtocol = 0.0004e4;
+
+    config.networkBaseTokenPriceInUsdProxyAggregator = 0xdE8d22d022261c9Fb4b5338DA8ceFb029175D0F5; //HYPE-USD pyth adapter
+    config.marketReferenceCurrencyPriceInUsdProxyAggregator = 0xdE8d22d022261c9Fb4b5338DA8ceFb029175D0F5; //HYPE-USD pyth adapter
+    config.wrappedNativeToken = 0x5555555555555555555555555555555555555555;
 
     return (roles, config, flags, deployedContracts);
   }
