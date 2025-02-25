1af8d56a8920d2e1bc7da4b6ffe83a47d4c316b4
fbslo <admin@fbslo.net>
2025-02-25 02:47:15 +0100

    fix: update Executor

--- src/contracts/extensions/v3-config-engine/Executor.sol
+++ src/contracts/extensions/v3-config-engine/Executor.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import {Ownable} from '../../dependencies/openzeppelin/contracts/Ownable.sol';
-import {AaveV3Payload} from './AaveV3Payload.sol';
 
 /**
  * @title HyperLend Governance Excutor
@@ -10,7 +9,21 @@ import {AaveV3Payload} from './AaveV3Payload.sol';
  */
 contract Executor is Ownable {
   function execute(address payload) external onlyOwner() {
-    (bool success,) = payload.delegatecall(abi.encodeWithSelector(AaveV3Payload.execute.selector));
-    require(success, 'execution failed');
+    (bool success,) = payload.delegatecall(abi.encodeWithSignature('execute()'));
+    if (!success) {
+      _revertWithReason(returndata);
+    }
+  }
+
+  
+  function _revertWithReason(bytes memory returndata) internal pure {
+    if (returndata.length > 0) {
+      assembly {
+        let returndata_size := mload(returndata)
+        revert(add(32, returndata), returndata_size)
+      }
+    } else {
+      revert("execution failed");
+    }
   }
 }
